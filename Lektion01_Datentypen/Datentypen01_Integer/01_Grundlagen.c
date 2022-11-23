7d7e9fe278dee199138c8e2bdb5ed610439677c4
Markus Konietzka <j0km0023@unibw.de>
2022-11-23 20:29:32 +0100

    Grundlagen

---
+++ Lektion01_Datentypen/Datentypen01_Integer/01_Grundlagen.c
@@ -0,0 +1,25 @@
+/**
+ * @author: Markus Konietzka
+ * @date: 23.11.2022
+ */
+
+int main() {
+    // Definition und Initialisierung
+    int a;
+    int b;
+    int c, d;
+    int u, v, w, x, y, z;
+
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+
+    // Rechnen mit Integer
+    u = a + b; // u = 3
+    v = u / (2 * 2); // v = 0
+    w = (a + b) * v; // 0
+    x = c++; // x = 3
+    y = d + (b * 3); // 10
+    z = a + b + c + d; // 11
+}
