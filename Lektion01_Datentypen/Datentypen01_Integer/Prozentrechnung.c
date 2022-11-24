f95e30c1547884c98a12d68856988abc76379f9d
Markus Konietzka <j0km0023@unibw.de>
2022-11-24 19:26:50 +0100

    Prozentrechnung

---
+++ Lektion01_Datentypen/Datentypen01_Integer/Prozentrechnung.c
@@ -0,0 +1,26 @@
+/**
+ * @author: Markus Konietzka
+ * @date: 24.11.2022
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int teil = 1;
+    int ganzes = 6;
+    int count;
+    int prozent;
+
+    /* Eingabe */
+    do {
+        fflush(stdin);
+        printf("Bitte 2 ganze Zalen für Teil und Ganzes (mit Komma getrennt) eingeben: ");
+        count = scanf("%d, %d", &teil, &ganzes);
+    } while (count != 2);
+
+    /* Ausgabe */
+    prozent = teil * 100 / ganzes;
+    printf("%d von %d entsprechen: %d %%\n", teil, ganzes, prozent);
+    return 0;
+}
