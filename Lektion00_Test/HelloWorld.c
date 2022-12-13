5ffea0820ace6b1b514a4736006596ec306f9ecd
Markus Konietzka <j0km0023@unibw.de>
2022-12-13 22:51:32 +0100

    Listen Implementierung in Test Ordner

--- Lektion00_Test/HelloWorld.c
+++ Lektion00_Test/HelloWorld.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+typedef struct list {
+    int zahl;
+    char zeichen;
+    struct list *next;
+} tList;
+
 int main() {
     printf("Hello, World!\n");
+
+    tList test1 = {123, 'a', NULL};
+    tList test2 = {456, 'b', &test1};
+
+    printf("%c: %d\n%c: %d", test2.zeichen, test2.zahl, test2.next->zeichen, test2.next->zahl);
     return 0;
 }
